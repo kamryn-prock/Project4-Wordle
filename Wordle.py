81a7472e49f3435567ac0e15dd674f51069da760
SpencerHJackson <90439665+SpencerHJackson@users.noreply.github.com>
2022-10-03 13:46:49 -0600

    Milestone 1

--- Wordle.py
+++ Wordle.py
@@ -13,22 +13,29 @@ import WordleDictionary
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
-selectedWord = ""
 
 def wordle():
+
+    def GetRandomWord() :
+        selectedWord = random.choice(FIVE_LETTER_WORDS)
+        print(selectedWord)
+        return selectedWord
+
+    def randwordtofirstRow() :
+        selectedWord = GetRandomWord()
+        for x in range(len(selectedWord)):
+            gw.set_square_letter(0, x, selectedWord[x])
 
     def enter_action(s):
         gw.show_message("You have to implement this method.")
 
     gw = WordleGWindow()
+    randwordtofirstRow()
     gw.add_enter_listener(enter_action)
+    
 
 # Startup code
 
 if __name__ == "__main__":
     wordle()
 
-def GetRandomWord() :
-    selectedWord = random.choice(FIVE_LETTER_WORDS)
-    print(selectedWord)
-    return selectedWord
